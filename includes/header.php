96c6ba23c348b53d8bba927f955a9a3d85ed6f24
SS11146111 <sahasangita59@gmail.com>
2024-08-22 21:42:11 +0530

    homepage phase3

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-md navbar-light">
+        <a class="navbar-brand" href="home.html">
+            <img src="img/logo.png" width="120" height="60"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#my-navbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="my-navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#signup-modal">
+                        <i class="fas fa-user"></i> Signup
+                    </a>
+                </li>
+                <div class="nav-vl"></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#login-modal">
+                        <i class="fas fa-sign-in-alt"></i>Login
+                    </a>
+                </li>
+            </ul>
+        </div>
+</nav>
